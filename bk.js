6e74732b86157e72d17c4bae72ea783dc4d4cc47
Wellford <well_fun@yahoo.com>
2017-09-23 02:23:46 -0700

    added gify api

--- bk.js
+++ bk.js
@@ -1,1 +1,15 @@
-document.getElementById("bkBtn").addEventListener("click", function(){alert('You just BKed'); });
+var gif = '';
+var bk = function(){
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://api.giphy.com/v1/gifs/search?api_key=MgTnHkUywbElTMCVRt24MH4FMQJJNzVG&q=hit button&limit=25&offset=0&rating=G&lang=en", false);
+	xhr.send();
+	var response = JSON.parse(xhr.response);
+	console.log(response);
+	gif = response.data[0].images.downsized_large.url;
+	document.getElementById("bk-gif").src=gif;
+	console.log(gif);
+	console.log(xhr);
+	alert("You just BKED");
+}
+
+document.getElementById("bkBtn").addEventListener("click", bk);
